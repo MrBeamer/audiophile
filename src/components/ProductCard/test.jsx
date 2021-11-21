89a51cc2bdc432ff8b524afc306936552507b6cd
michael beamer <beamermichael@gmail.com>
2021-11-21 02:02:03 +0100

    added mirrored category card component

--- src/components/ProductCard/test.jsx
+++
@@ -1,3 +0,0 @@
-const imgRight = right ? { gridColumn: "2", gridColumn: "1" } : "";
-
-const textLeft = left ? { gridRow: "1" } : "";
--- src/pages/CategoryHeadphones.jsx
+++ src/pages/CategoryHeadphones.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CategoryBanner from "../components/CategoryBanner/CategoryBanner";
 import ProductCard from "../components/ProductCard/ProductCard";
+import CategoryCards from "../components/CategoryCards/CategoryCards";
+import ShopIntro from "../components/ShopIntro/ShopIntro";
 
 export default function CategoryHeadphones() {
   return (
@@ -17,8 +19,16 @@ export default function CategoryHeadphones() {
           imgPath="images/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
           title="XX99 Mark II Headphones"
           paragraph="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
-          styles="mirror"
+          styles="mirrored"
         />
+
+        <ProductCard
+          imgPath="images/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg"
+          title="XX99 Mark II Headphones"
+          paragraph="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
+        />
+        <CategoryCards style={{ marginTop: "0", marginBottom: "0" }} />
+        <ShopIntro />
       </div>
     </>
   );
