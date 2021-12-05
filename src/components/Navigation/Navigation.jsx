82cdd003cd0ed93a26e2e98a7bdb2e2bbd8c78e2
michael beamer <beamermichael@gmail.com>
2021-12-05 12:38:13 +0100

    mobile navigation fully added and home page finish

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,43 +10,51 @@ export default function Navigation(props) {
   const context = useContext(ModalContext);
   const { cart } = props;
   const sumQuantity = cart?.reduce((sum, product) => sum + product.quantity, 0);
+  console.log(context.isShowing.navbar);
   return (
-    <nav className="nav">
-      <div className="nav__mobile--menu">
-        <i className="fas fa-bars"></i>
-        <Logo />
-      </div>
-      <div className="nav__nav-links collapsed">
-        <NavLink className="nav__nav-link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="nav__nav-link" to="headphones">
-          Headphones
-        </NavLink>
-        <NavLink className="nav__nav-link" to="/speakers">
-          Speakers
-        </NavLink>
-        <NavLink className="nav__nav-link" to="/earphones">
-          Earphones
-        </NavLink>
-      </div>
-      {cart.length > 0 ? (
-        <div className="nav__cart-quantity">{sumQuantity}</div>
-      ) : null}
-      <img
-        className="nav__cart-icon"
-        onClick={() => context.toggleModal("cart")}
-        src="/images/icon-cart.svg"
-        alt="shopping-cart icon"
-      />
+    <>
+      <nav className="nav">
+        <div className="nav__mobile--menu">
+          <i
+            className="fas fa-bars"
+            onClick={() => context.toggleModal("navbar")}
+          ></i>
+          <Logo />
+        </div>
+        <div className="nav__nav-links collapsed">
+          <NavLink className="nav__nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav__nav-link" to="headphones">
+            Headphones
+          </NavLink>
+          <NavLink className="nav__nav-link" to="/speakers">
+            Speakers
+          </NavLink>
+          <NavLink className="nav__nav-link" to="/earphones">
+            Earphones
+          </NavLink>
+        </div>
+        {cart.length > 0 ? (
+          <div className="nav__cart-quantity">{sumQuantity}</div>
+        ) : null}
+        <img
+          className="nav__cart-icon"
+          onClick={() => context.toggleModal("cart")}
+          src="/images/icon-cart.svg"
+          alt="shopping-cart icon"
+        />
+      </nav>
       {context.isShowing.navbar ? (
         <>
           <div className="nav__mask animate__animated animate__fadeIn"></div>
-          <div className="nav__mobile">
-            <CategoryCards></CategoryCards>
-          </div>
+          <nav className="nav__mobile-wrapper animate__animated animate__fadeIn">
+            <div className="nav__mobile">
+              <CategoryCards></CategoryCards>
+            </div>
+          </nav>
         </>
       ) : null}
-    </nav>
+    </>
   );
 }
