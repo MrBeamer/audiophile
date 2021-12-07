ba0d4be5c54a00dfce5295e79f06038ed6dcb8e2
michael beamer <beamermichael@gmail.com>
2021-12-07 12:19:45 +0100

    added mobile product detail page

--- src/components/ProductGallery/ProductGallery.jsx
+++ src/components/ProductGallery/ProductGallery.jsx
@@ -5,19 +5,50 @@ export default function ProductGallery({ product }) {
   return (
     <div className="productGallery">
       <img
-        className="productGallery__img--1"
-        src={product.image?.gallery1}
+        className="productGallery__img--1 desktop-image"
+        src={product.image?.galleryDesktop1}
         alt={`${product.name} ${product.category} `}
       />
       <img
-        className="productGallery__img--2"
-        src={product.image?.gallery2}
+        className="productGallery__img--1 tablet-image"
+        src={product.image?.galleryTablet1}
+        alt={`${product.name} ${product.category} `}
+      />
+      <img
+        className="productGallery__img--1 mobile-image"
+        src={product.image?.galleryMobile1}
         alt={`${product.name} ${product.category} `}
       />
 
       <img
-        className="productGallery__img--3"
-        src={product.image?.gallery3}
+        className="productGallery__img--2 desktop-image"
+        src={product.image?.galleryDesktop2}
+        alt={`${product.name} ${product.category} `}
+      />
+      <img
+        className="productGallery__img--2 tablet-image"
+        src={product.image?.galleryTablet2}
+        alt={`${product.name} ${product.category} `}
+      />
+      <img
+        className="productGallery__img--2 mobile-image"
+        src={product.image?.galleryMobile2}
+        alt={`${product.name} ${product.category} `}
+      />
+
+      <img
+        className="productGallery__img--3 desktop-image"
+        src={product.image?.galleryDesktop3}
+        alt={`${product.name} ${product.category} `}
+      />
+      <img
+        className="productGallery__img--3 tablet-image"
+        src={product.image?.galleryTablet3}
+        alt={`${product.name} ${product.category} `}
+      />
+      <img
+        className="productGallery__img--3 mobile-image"
+        src={product.image?.galleryMobile3}
         alt={`${product.name} ${product.category} `}
       />
     </div>
